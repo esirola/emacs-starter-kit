dbed4787411a974472940d6514dd5db84e2d3e14
Enrico Sirola <enrico.sirola@gmail.com>
2012-03-30 09:43:27 +0200

    pyflakes -> pylint

--- es-python.el
+++ es-python.el
@@ -10,17 +10,30 @@
   (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
   (autoload 'python-mode "python-mode"))
 
+;; (when (load "flymake" t)
+;;   ;; python support
+;;   (defun flymake-pyflakes-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list "pyflakes" (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pyflakes-init))
+;;   (add-hook 'find-file-hook 'flymake-find-file-hook))
+
 (when (load "flymake" t)
   ;; python support
-  (defun flymake-pyflakes-init ()
+  (defun flymake-epylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+      (list "epylint" (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init))
+               '("\\.py\\'" flymake-epylint-init))
   (add-hook 'find-file-hook 'flymake-find-file-hook))
 
 (message "Python loaded!")
