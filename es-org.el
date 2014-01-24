63b774c54e77ca9a57e10cce6fd00404580996dc
Enrico Sirola <enrico.sirola@gmail.com>
2014-01-24 15:40:43 +0100

    org journal

--- es-org.el
+++ es-org.el
@@ -9,7 +9,6 @@
   (global-set-key (kbd "C-c l") 'org-store-link)
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c r") 'org-remember)
-  (org-remember-insinuate)
   (setq org-mobile-directory "~/Dropbox/MobileOrg"
         org-directory "~/Dropbox/org/"
         org-mobile-inbox-for-pull "~/Dropbox/org/from-mobile.org"
@@ -18,6 +17,12 @@
 
 (setq org-hide-leading-stars t
       org-level-color-stars-only nil)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline (concat org-directory "/todo.org") "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree (concat org-directory "/journal.org"))
+         "* %?\nEntered on %U\n  %i\n  %a")))
 
 (message "org loaded!")
 
