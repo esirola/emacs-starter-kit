e44ac54bb01370159bc870e1d6cb4f16b85d16c5
Enrico Sirola <enrico.sirola@gmail.com>
2014-03-26 15:03:47 +0100

    nrepl->cider

--- es-clojure.el
+++ es-clojure.el
@@ -1,35 +1,35 @@
 ;;; ----------------------------------------------------------------------
 ;;; CLOJURE
 ;;; ----------------------------------------------------------------------
+(ignore-errors
+  (require 'es-common)
+  (require 'paredit)
+  (require 'clojure-mode)
+  (require 'cider)
+  (require 'ac-nrepl)
 
-(require 'es-common)
-(require 'paredit)
-(require 'clojure-mode)
-(require 'nrepl)
-(require 'ac-nrepl)
+  (eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'nrepl-mode))
 
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
+  (add-hook 'clojure-mode-hook 'paredit-mode)
+  (add-hook 'clojure-mode-hook 'set-newline-and-indent)
+  (add-hook 'nrepl-interaction-mode-hook 'cider-turn-on-eldoc-mode)
+  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'set-newline-and-indent)
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+  (add-hook 'cider-repl-mode-hook 'subword-mode)
+  (add-hook 'cider-repl-mode-hook 'paredit-mode)
+  (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
 
-(add-hook 'nrepl-mode-hook 'subword-mode)
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+  (defun set-auto-complete-as-completion-at-point-function ()
+    (setq completion-at-point-functions '(auto-complete)))
 
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
+  (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+                                        ;(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
-;(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
-
-(message "Clojure loaded!")
+  (message "Clojure loaded!"))
 
 (provide 'es-clojure)
